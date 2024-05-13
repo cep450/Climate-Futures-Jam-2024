872ed9357bf50a91af137c8bde1d69d05de6077d
PatFantasia <desouza.paatrice@gmail.com>
2024-05-13 20:53:33 +0100

    feat: Brute implementation of Tutorial dialogue

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using PixelCrushers.DialogueSystem;
 
 public class DialogueManager : MonoBehaviour
 {
+    private QuestState _tutorialQuest;
+    QuestCondition _condition;
+    [SerializeField] private DialogueDatabase _gameDatabase;
+    private Variable _canStartTutorialDialogue;
+
     // Start is called before the first frame update
     void Start()
     {
+        //_canStartTutorialDialogue = _gameDatabase.GetVariable("IsTutorialDialogueActive");
+        DialogueLua.SetVariable("IsTutorialDialogueActive", false);
+        //_tutorialQuest.
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    static public void OnConversationStart()
     {
-        
+
+    }
+    static public void OnConversationEnd()
+    {
+
+    }
+    static public void StartQuest()
+    {
+
+    }
+    static public void StartTutorialDialogue()
+    {
+        DialogueLua.SetVariable("IsTutorialDialogueActive", true);
+        Debug.Log("ISTut " +  DialogueLua.GetVariable("IsTutorialDialogueActive").asBool);
+        //var o = new object();
+        //o.GetType();
     }
 }
--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button _quitButton;
+    void Awake()
     {
-        
+        _quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
