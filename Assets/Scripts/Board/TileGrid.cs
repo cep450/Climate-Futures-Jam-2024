7cb6dadebdfafb6de3e01444accae9f87009074c
pc <61981562+cep450@users.noreply.github.com>
2024-05-17 23:42:13 -0400

    enemy and ally prefab setup

---
+++ Assets/Scripts/Board/TileGrid.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGrid : WorldGrid<Tile>
+{
+	/*
+		Because Unity won't let you add generic type classes as components
+	*/
+}
